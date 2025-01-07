86bf73511d6c975887d91e9d5adab2c457efb93e
Mayur Gurav <mayur.easternts@gmail.com>
2025-01-07 12:20:04 +0530

    initial commit

--- resources/views/vote.blade.php
+++ resources/views/vote.blade.php
@@ -75,3 +75,10 @@
     </div>
 </body>
 </html>
+<!--git init
+git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/Devang9898/Kombee-Daily-Task
+git branch -M main
+git push -u origin main
+-->
